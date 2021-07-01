1b4af8842cc77b6110f667474bd57cd72f733004
JessicaWeiss <jessica.weiss@uhnresearch.ca>
2021-07-01 18:08:24 -0400

    Testing

--- R/testing.R
+++ R/testing.R
@@ -1,29 +1,151 @@
-# ## No changes all positive
-# df <- data.frame(id=c(1,2,3,4,5),start=c(0,0,0,0,0),end=c(1,2,3,4,5))
-# swimmer_plot(df,name_fill='id')
-#
-# df <- data.frame(id=c(2,1,3,4,5),start=c(0,0,0,0,0),end=c(1,6,3,4,5))
-# swimmer_plot(df,name_fill='id')
-#
-# ## No changes all negative
-# df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,-3,-2,-1),end=c(1,2,3,4,5))
-# swimmer_plot(df,name_fill='id')
-# ## No changes some negative some positive
-#
-# df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,0,1,2),end=c(1,2,3,4,5),col=c(1,2,3,4,5))
-# swimmer_plot(df,name_fill='col')
-#
-# df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,-5,1,2),end=c(5,2,3,4,5),col=c(1,2,3,4,5))
-# swimmer_plot(df,name_fill='col')
-#
-#
-#
-# ## Changes all positive
-#
-# ##Changes all negative
-#
-# ##Changes some negative some positive
-#
-#
-#
-# # swim_plot
+
+
+
+
+
+# No changes all positive -------------------------------------------------
+df <- data.frame(id=c(1,2,3,4,5),start=c(0,0,0,0,0),end=c(1,2,3,4,5),group1=c(5,5,5,10,10),group2=c('A',"B","B","A","C"))
+
+
+##BASIC
+  swimmer_plot(df)
+##FILL BY ID
+  swimmer_plot(df,name_fill='id')
+##FILL BY CONTINUOUS
+  swimmer_plot(df,name_fill='group1')
+##FILL BY CATEGORIC
+  swimmer_plot(df,name_fill='group2')
+##ALPHA
+  swimmer_plot(df,name_alpha='group1')
+  swimmer_plot(df,name_alpha='group2')
+##COL
+  swimmer_plot(df,name_col='group2')
+##ORDER BY VARIABLE
+  swimmer_plot(df,id_order='group2',name_fill='group2')
+  swimmer_plot(df,id_order='group2',increasing = F,name_fill='group2')
+##ORDER
+  swimmer_plot(df,id_order=c(4,5,1,2,3),name_fill='group2')
+
+##STRATIFY
+  swimmer_plot(df,name_fill='group2',stratify = 'group1')
+  swimmer_plot(df,name_fill='group2',stratify = c('group1','group2'))
+
+
+# No changes all negative, end positive -----------------------------------
+df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,-3,-2,-1),end=c(1,2,3,4,5),group1=c(5,5,5,10,10),group2=c('A',"B","B","A","C"))
+
+##BASIC
+swimmer_plot(df)
+##FILL BY ID
+swimmer_plot(df,name_fill='id')
+##FILL BY CONTINUOUS
+swimmer_plot(df,name_fill='group1')
+##FILL BY CATEGORIC
+swimmer_plot(df,name_fill='group2')
+##ALPHA
+swimmer_plot(df,name_alpha='group1')
+swimmer_plot(df,name_alpha='group2')
+##COL
+swimmer_plot(df,name_col='group2')
+##ORDER BY VARIABLE
+swimmer_plot(df,id_order='group2',name_fill='group2')
+swimmer_plot(df,id_order='group2',increasing = F,name_fill='group2')
+##ORDER
+swimmer_plot(df,id_order=c(4,5,1,2,3),name_fill='group2')
+
+##STRATIFY
+swimmer_plot(df,name_fill='group2',stratify = 'group1')
+swimmer_plot(df,name_fill='group2',stratify = c('group1','group2'))
+
+
+
+# No changes all negative end at zero -------------------------------------
+df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,-3,-2,-1),end=c(0,0,0,0,0),group1=c(5,5,5,10,10),group2=c('A',"B","B","A","C"))
+
+##BASIC
+swimmer_plot(df)
+##FILL BY ID
+swimmer_plot(df,name_fill='id')
+##FILL BY CONTINUOUS
+swimmer_plot(df,name_fill='group1')
+##FILL BY CATEGORIC
+swimmer_plot(df,name_fill='group2')
+##ALPHA
+swimmer_plot(df,name_alpha='group1')
+swimmer_plot(df,name_alpha='group2')
+##COL
+swimmer_plot(df,name_col='group2')
+##ORDER BY VARIABLE
+swimmer_plot(df,id_order='group2',name_fill='group2')
+swimmer_plot(df,id_order='group2',increasing = F,name_fill='group2')
+##ORDER
+swimmer_plot(df,id_order=c(4,5,1,2,3),name_fill='group2')
+
+##STRATIFY
+swimmer_plot(df,name_fill='group2',stratify = 'group1')
+swimmer_plot(df,name_fill='group2',stratify = c('group1','group2'))
+
+# No changes all end negative ---------------------------------------------
+df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,-3,-2,-1),end=c(-1,-1,-1,-1,-0.5),col=c(1,2,3,4,5),group1=c(5,5,5,10,10),group2=c('A',"B","B","A","C"))
+
+##BASIC
+swimmer_plot(df)
+##FILL BY ID
+swimmer_plot(df,name_fill='id')
+##FILL BY CONTINUOUS
+swimmer_plot(df,name_fill='group1')
+##FILL BY CATEGORIC
+swimmer_plot(df,name_fill='group2')
+##ALPHA
+swimmer_plot(df,name_alpha='group1')
+swimmer_plot(df,name_alpha='group2')
+##COL
+swimmer_plot(df,name_col='group2')
+##ORDER BY VARIABLE
+swimmer_plot(df,id_order='group2',name_fill='group2')
+swimmer_plot(df,id_order='group2',increasing = F,name_fill='group2')
+##ORDER
+swimmer_plot(df,id_order=c(4,5,1,2,3),name_fill='group2')
+
+##STRATIFY
+swimmer_plot(df,name_fill='group2',stratify = 'group1')
+swimmer_plot(df,name_fill='group2',stratify = c('group1','group2'))
+
+# No changes some positive some negative ----------------------------------
+
+df <- data.frame(id=c(1,2,3,4,5),start=c(-5,-4,0,1,2),end=c(-1,2,3,4,5),col=c(1,2,3,4,5),group1=c(5,5,5,10,10),group2=c('A',"B","B","A","C"))
+
+##BASIC
+swimmer_plot(df)
+##FILL BY ID
+swimmer_plot(df,name_fill='id')
+##FILL BY CONTINUOUS
+swimmer_plot(df,name_fill='group1')
+##FILL BY CATEGORIC
+swimmer_plot(df,name_fill='group2')
+##ALPHA
+swimmer_plot(df,name_alpha='group1')
+swimmer_plot(df,name_alpha='group2')
+##COL
+swimmer_plot(df,name_col='group2')
+##ORDER BY VARIABLE
+swimmer_plot(df,id_order='group2',name_fill='group2')
+swimmer_plot(df,id_order='group2',increasing = F,name_fill='group2')
+##ORDER
+swimmer_plot(df,id_order=c(4,5,1,2,3),name_fill='group2')
+
+##STRATIFY
+swimmer_plot(df,name_fill='group2',stratify = 'group1')
+swimmer_plot(df,name_fill='group2',stratify = c('group1','group2'))
+
+# Changes all positive ----------------------------------------------------
+
+
+
+# Changes crossing 0 ------------------------------------------------------
+
+
+
+
+
+
