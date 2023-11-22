5a385cc43188ac0cde99cc7366cad8f03012d809
tylerpittman <pittman.tyler@gmail.com>
2023-11-22 10:15:55 -0500

    Document simulated datasets ae, res and arm

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Clinical Trial: Adverse events
 #'
-#' A dataset containing the adverse event information from a simulated clinical trial
+#' A dataset containing the adverse event information from a simulated clinical trial.
 #'
 #' @format A data frame with 11 rows and 6 variables:
 #' \describe{
@@ -14,11 +14,9 @@
 #' }
 "ClinicalTrial.AE"
 
-
-
 #' Clinical Trial: Treatment
 #'
-#'A dataset containing the treatment arm information from a simulated clinical trial
+#'A dataset containing the treatment arm information from a simulated clinical trial.
 #'
 #' @format A data frame with 53 rows and 6 variables:
 #' \describe{
@@ -34,7 +32,7 @@
 
 #' Clinical Trial: Response
 #'
-#' A dataset containing the response information from a simulated clinical trial
+#' A dataset containing the response information from a simulated clinical trial.
 #'
 #' @format A data frame with 36 rows and 7 variables:
 #' \describe{
@@ -49,12 +47,9 @@
 #' }
 "ClinicalTrial.Response"
 
-
-
-
 #' Clinical Trial: Stage
 #'
-#' A dataset containing the Stage information from a simulated clinical trial
+#' A dataset containing the Stage information from a simulated clinical trial.
 #'
 #' @format A data frame with 36 rows and 2 variables:
 #' \describe{
@@ -63,3 +58,53 @@
 #'
 #' }
 "ClinicalTrial.Stage"
+
+#' Adverse events
+#'
+#' A dataset containing adverse events from a simulated clinical trial.
+#'
+#' @format A data frame with 87 rows and 7 variables:
+#' \describe{
+#'   \item{SUBJECT}{Patient id}
+#'   \item{COURSE_NUM}{Treatment course cycle from which adverse event occurred}
+#'   \item{EVENT}{Treatment course cycle from which adverse event occurred}
+#'   \item{ARM}{Treatment arm for participant}
+#'   \item{SITE}{Hospital site for participant}
+#'   \item{SEX}{Patient sex}
+#'   \item{AGE}{Patient age at AE onset date}
+#' }
+"ae"
+
+#' Treatment course
+#'
+#' A dataset containing treatment course from a simulated clinical trial.
+#'
+#' @format A data frame with 47 rows and 7 variables:
+#' \describe{
+#'   \item{SUBJECT}{Patient id}
+#'   \item{ARM}{Treatment arm for participant}
+#'   \item{SITE}{Hospital site for participant}
+#'   \item{SEX}{Patient sex}
+#'   \item{AGE}{Patient age at AE onset date}
+#'   \item{END_TRT}{Last treatment course cycle for patient}
+#'   \item{CONTINUED_TREATMENT}{Denotes if patient continued treatment after last treatment cycle on study}
+#' }
+"arm"
+
+#' Response
+#'
+#' A dataset containing observed responses in treatment course cycles from a simulated clinical trial.
+#'
+#' @format A data frame with 67 rows and 9 variables:
+#' \describe{
+#'   \item{SUBJECT}{Patient id}
+#'   \item{ARM}{Treatment arm for participant}
+#'   \item{SITE}{Hospital site for participant}
+#'   \item{RESPONSE_START}{Treatment course cycle denoting start of response}
+#'   \item{RESPONSE_END}{Treatment course cycle denoting end of response}
+#'   \item{RESPONSE}{Observed response for patient at treatment course cycle}
+#'   \item{RESPONSE}{Observed response for patient at treatment course cycle}
+#'   \item{CONTINUED_RESPONSE}{Denotes if response was continued after last observation at treatment course cycle}
+#'   \item{AGE}{Patient age at treatment course cycle}
+#' }
+"res"
